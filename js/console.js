0c321508ecfbe922efc10415a1e0ef858c8a3f77
extempl <extempl@gmail.com>
2015-06-19 08:33:54 +0300

    Delete console.js

--- js/console.js
+++
@@ -1,9 +0,0 @@
-function console(hash) {
-    var params = 'menubar=no,location=no,resizable=no,scrollbars=no,status=no,' +
-                 'height=400,width=500,toolbar=no,top=50,left=50';
-    console_win = window.open('/?console=' + hash, 'Console', params);
-    if(console_win == null)
-        alert('отключите блокировку всплывающих окон');
-    else
-        console_win.focus();
-}
